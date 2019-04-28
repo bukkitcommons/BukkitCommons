ce12351fab63dfd8449001f4870e6b130f1e8f07
Sotr <i@omc.hk>
2019-04-28 21:27:12 +0800

    Add more static helpers

--- src/main/java/org/bukkit/commons/BukkitCommons.java
+++ src/main/java/org/bukkit/commons/BukkitCommons.java
@@ -2,22 +2,35 @@ package org.bukkit.commons;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.function.Consumer;
 
+import javax.annotation.Nonnull;
+
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Statistic;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public final class BukkitCommons {
+    // ------------------------------
+    // Bukkit
+    // ------------------------------
+    
     /**
      * 
      * @param consumer
      * @return
      */
-    public Collection<? extends Player> forEachOnlinePlayers(Consumer<Player> consumer) {
+    public Collection<? extends Player> forEachOnlinePlayers(@Nonnull Consumer<Player> consumer) {
         Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
         onlinePlayers.forEach(consumer);
         return onlinePlayers;
@@ -35,5 +48,65 @@ public final class BukkitCommons {
         maxPlayersField.setAccessible(true);
         maxPlayersField.set(playerListField.get(Bukkit.getServer()), maxPlayers);
         return maxPlayers;
+    }
+    
+    /**
+     * 
+     * @param consumer
+     * @return
+     */
+    public Collection<OfflinePlayer> forEachWhitelistedPlayers(@Nonnull Consumer<OfflinePlayer> consumer) {
+        Collection<OfflinePlayer> onlinePlayers = Bukkit.getWhitelistedPlayers();
+        onlinePlayers.forEach(consumer);
+        return onlinePlayers;
+    }
+    
+    /**
+     * 
+     * @param message
+     * @return
+     */
+    public static String broadcast(@Nonnull String message) {
+        Bukkit.broadcastMessage(message);
+        return message;
+    }
+    
+    /**
+     * 
+     * @param name
+     * @return
+     */
+    public static Optional<Player> getPlayer(@Nonnull String name) {
+        return Optional.ofNullable(Bukkit.getPlayerExact(name));
+    }
+    
+    /**
+     * 
+     * @param uuid
+     * @return
+     */
+    public static Optional<Player> getPlayer(@Nonnull UUID uuid) {
+        return Optional.ofNullable(Bukkit.getPlayer(uuid));
+    }
+    
+    /**
+     * 
+     * @param name
+     * @return
+     */
+    public static Optional<UUID> getPlayerUniqueId(@Nonnull String name) {
+        Optional<Player> player = getPlayer(name);
+        return player.isPresent() ? Optional.of(player.get().getUniqueId()) : Optional.empty();
+    }
+    
+    /**
+     * 
+     * @param consumer
+     * @return
+     */
+    public static List<World> forEachWorlds(Consumer<World> consumer) {
+        List<World> worlds = Bukkit.getWorlds();
+        worlds.forEach(consumer);
+        return worlds;
     }
 }
