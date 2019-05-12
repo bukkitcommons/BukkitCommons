0417563fa8475a93e16d85b4dd2016ea205a1640
Ghost_chu <2908803755@qq.com>
2019-05-12 17:30:49 +0800

    inRegion2D, inRegion3D

--- src/main/java/cc/bukkit/world/LocationUtils.java
+++ src/main/java/cc/bukkit/world/LocationUtils.java
@@ -1,6 +1,9 @@
 package cc.bukkit.world;
 
+import org.bukkit.Location;
 import org.bukkit.block.BlockFace;
+
+import cc.bukkit.math.NumberUtils;
 
 public class LocationUtils {
 	/**
@@ -19,4 +22,79 @@ public class LocationUtils {
 			return BlockFace.WEST;
 		}
 	}
+	/**
+	 * Check a Location is in a region or not, include the region border
+	 * @param Location needCheckLocation, Location regionLocation1, Location regionLocation2
+	 * @return boolean inORnot
+	 */
+	public static boolean inRegion3D_checkBorder(Location needCheckLocation, Location loc1, Location loc2) {
+		int loc1X = loc1.getBlockX();
+		int loc1Y = loc1.getBlockY();
+		int loc1Z = loc1.getBlockZ();
+		int loc2X = loc2.getBlockX();
+		int loc2Y = loc2.getBlockY();
+		int loc2Z = loc2.getBlockZ();
+		if(!NumberUtils.between_checkBorder(needCheckLocation.getBlockX(), loc1X, loc2X))
+			return false;
+		if(!NumberUtils.between_checkBorder(needCheckLocation.getBlockY(), loc1Y, loc2Y))
+			return false;
+		if(!NumberUtils.between_checkBorder(needCheckLocation.getBlockY(), loc1Z, loc2Z))
+			return false;
+		return true;
+		
+	}
+	/**
+	 * Check a Location is in a region or not, exclude the region border
+	 * @param Location needCheckLocation, Location regionLocation1, Location regionLocation2
+	 * @return boolean inORnot
+	 */
+	public static boolean inRegion3D(Location needCheckLocation, Location loc1, Location loc2) {
+		int loc1X = loc1.getBlockX();
+		int loc1Y = loc1.getBlockY();
+		int loc1Z = loc1.getBlockZ();
+		int loc2X = loc2.getBlockX();
+		int loc2Y = loc2.getBlockY();
+		int loc2Z = loc2.getBlockZ();
+		if(!NumberUtils.between(needCheckLocation.getBlockX(), loc1X, loc2X))
+			return false;
+		if(!NumberUtils.between(needCheckLocation.getBlockY(), loc1Y, loc2Y))
+			return false;
+		if(!NumberUtils.between(needCheckLocation.getBlockY(), loc1Z, loc2Z))
+			return false;
+		return true;
+	}
+	/**
+	 * Check a Location is in a region or not, include the region border
+	 * @param Location needCheckLocation, Location regionLocation1, Location regionLocation2
+	 * @return boolean inORnot
+	 */
+	public static boolean inRegion2D_checkBorder(Location needCheckLocation, Location loc1, Location loc2) {
+		int loc1X = loc1.getBlockX();
+		int loc1Z = loc1.getBlockZ();
+		int loc2X = loc2.getBlockX();
+		int loc2Z = loc2.getBlockZ();
+		if(!NumberUtils.between_checkBorder(needCheckLocation.getBlockX(), loc1X, loc2X))
+			return false;
+		if(!NumberUtils.between_checkBorder(needCheckLocation.getBlockY(), loc1Z, loc2Z))
+			return false;
+		return true;
+		
+	}
+	/**
+	 * Check a Location is in a region or not, exclude the region border
+	 * @param Location needCheckLocation, Location regionLocation1, Location regionLocation2
+	 * @return boolean inORnot
+	 */
+	public static boolean inRegion2D(Location needCheckLocation, Location loc1, Location loc2) {
+		int loc1X = loc1.getBlockX();
+		int loc1Z = loc1.getBlockZ();
+		int loc2X = loc2.getBlockX();
+		int loc2Z = loc2.getBlockZ();
+		if(!NumberUtils.between(needCheckLocation.getBlockX(), loc1X, loc2X))
+			return false;
+		if(!NumberUtils.between(needCheckLocation.getBlockY(), loc1Z, loc2Z))
+			return false;
+		return true;
+	}
+	
 }
