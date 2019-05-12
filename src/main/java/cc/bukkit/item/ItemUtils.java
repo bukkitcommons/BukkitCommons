cb8c087541b6180c04e83c0151a1ba67d9997a39
Ghost_chu <2908803755@qq.com>
2019-05-12 17:05:24 +0800

    item serialize and deserialize

--- src/main/java/cc/bukkit/item/ItemUtils.java
+++ src/main/java/cc/bukkit/item/ItemUtils.java
@@ -2,6 +2,8 @@ package cc.bukkit.item;
 
 import java.text.DecimalFormat;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 
@@ -18,5 +20,27 @@ public class ItemUtils {
 		double max = item.getType().getMaxDurability();
 		DecimalFormat formatter = new DecimalFormat("0");
 		return formatter.format((1 - dura / max) * 100.0);
+	}
+	/**
+	 * Covert ItemStack to YAML string.
+	 * @param ItemStack iStack
+	 * @return String serialized itemStack
+	 */
+	public static String serialize(ItemStack iStack) {
+		YamlConfiguration cfg = new YamlConfiguration();
+		cfg.set("item", iStack);
+		return cfg.saveToString();
+	}
+	/**
+	 * Covert YAML string to ItemStack.
+	 * @param String serialized itemStack
+	 * @return ItemStack iStack
+	 */
+	public static ItemStack deserialize(String config) throws InvalidConfigurationException {
+		YamlConfiguration cfg = new YamlConfiguration();
+		cfg.loadFromString(config);
+		cfg.getString("item");
+		ItemStack stack = cfg.getItemStack("item");
+		return stack;
 	}
 }
