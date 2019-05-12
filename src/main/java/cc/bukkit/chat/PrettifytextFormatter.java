d5f1f21810e0b19d3f51dd4aa74106f5ff69e264
Ghost_chu <2908803755@qq.com>
2019-05-12 17:43:05 +0800

    PrettifyText Formatter

---
+++ src/main/java/cc/bukkit/chat/PrettifytextFormatter.java
@@ -0,0 +1,33 @@
+package cc.bukkit.chat;
+
+public class PrettifytextFormatter {
+	/**
+	 * Converts a name like IRON_INGOT into Iron Ingot to improve readability
+	 * 
+	 * @param ugly
+	 *            The string such as IRON_INGOT
+	 * @return A nicer version, such as Iron Ingot
+	 * 
+	 */
+	public static String prettifyText(String ugly) {
+		String[] nameParts = ugly.split("_");
+		if (nameParts.length==1) {
+			return firstUppercase(ugly);
+		}
+
+		StringBuilder sb=new StringBuilder();
+		for (String part : nameParts) {
+			sb.append(firstUppercase(part)+" ");
+		}
+
+		return sb.toString();
+	}
+	
+	public static String firstUppercase(String string) {
+		if (string.length()>1) {
+			return Character.toUpperCase(string.charAt(0))+string.substring(1).toLowerCase();
+		} else {
+			return string.toUpperCase();
+		}
+	}
+}
