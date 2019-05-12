8301b4c083891859e4018c341bb33d738adbce58
Ghost_chu <2908803755@qq.com>
2019-05-12 17:37:29 +0800

    ItemJson

---
+++ src/main/java/cc/bukkit/item/ItemJson.java
@@ -0,0 +1,45 @@
+package cc.bukkit.item;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.Method;
+
+public abstract class ItemJson { 
+    private static Method craftItemStack_asNMSCopyMethod;
+    private static Class<?> nbtTagCompoundClass;
+    private static Method itemStack_saveMethod;
+    
+    static {
+        String name = Bukkit.getServer().getClass().getPackage().getName();
+        String nmsVersion = name.substring(name.lastIndexOf('.') + 1);
+        
+        try {
+            craftItemStack_asNMSCopyMethod = Class.forName("org.bukkit.craftbukkit." + nmsVersion + ".inventory.CraftItemStack")
+                    .getDeclaredMethod("asNMSCopy", ItemStack.class);
+            
+            nbtTagCompoundClass = Class.forName("net.minecraft.server." + nmsVersion + ".NBTTagCompound");
+            
+            itemStack_saveMethod = Class.forName("net.minecraft.server." + nmsVersion + ".ItemStack")
+                    .getDeclaredMethod("save", nbtTagCompoundClass);
+            
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+    /**
+     * 
+     * @param ItemStack
+     * @return ItemStack's json, can use on net.md_5.bungee.chat.
+     */
+	public static String saveJsonfromNMS(ItemStack bStack) throws Throwable {
+	    if (bStack.getType() == Material.AIR)
+	        return null;
+        Object mcStack = craftItemStack_asNMSCopyMethod.invoke(null, bStack);
+        Object nbtTagCompound = nbtTagCompoundClass.newInstance();
+        
+        itemStack_saveMethod.invoke(mcStack, nbtTagCompound);
+        return nbtTagCompound.toString();
+	}
+}
