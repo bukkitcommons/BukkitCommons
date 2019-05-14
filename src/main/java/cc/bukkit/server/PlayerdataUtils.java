9aa6cfb9f4bd9cd578138a5f2c984f2169c2c2bd
Sotr <i@omc.hk>
2019-05-15 03:19:51 +0800

    Finish offline player

--- src/main/java/cc/bukkit/server/PlayerdataUtils.java
+++ src/main/java/cc/bukkit/server/PlayerdataUtils.java
@@ -2,12 +2,12 @@ package cc.bukkit.server;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+
+import lombok.SneakyThrows;
 
 public class PlayerdataUtils {
 	/**
@@ -30,37 +30,42 @@ public class PlayerdataUtils {
 			return null;
 		return playerdataFile;
 	}
-	 /**
-    *
-    * @param uuid
-    * @return
-    */
+	
 
-   public static Player getOfflinePlayer(UUID uuid) {
-       String version = "v" + Bukkit.getServer().getClass().getPackage().getName().split("v")[1];
-       Class<?> gameProfileClass = Class.forName("net.minecraft.server."+version+".GameProfile");
-       
-       Constructor<?> gameProfileConstructor = gameProfileClass.getConstructor(UUID.class,OfflinePlayer.class);
-       Object gameProfile = gameProfileConstructor.newInstance(uuid,Bukkit.getOfflinePlayer(uuid).getName());
-       // [SOLVED] GameProfile profile = new GameProfile(uuid,Bukkit.getOfflinePlayer(uuid));
-       Class<?> minecraftServer = Class.forName("net.minecraft.server."+version+".MinecraftServer");
-       Object serverInstance = ServerTPS.getNMSClass("MinecraftServer").getMethod("getServer").invoke(null);
-       // [SOLVED]MinecraftServer server = ((CraftServer) Bukkit.getServer()).getServer();
-       //EntityPlayer entity;
-       Class<?> entityClass = Class.forName("net.minecraft.server."+version+".EntityPlayer");
-       Object entity;
-       if(version.equals("v1_14_R1")){
-    	   
-           //entity = new EntityPlayer(server, server.getWorldServer(DimensionManager.a(0)), profile, new PlayerInteractManager(server.getWorldServer(DimensionManager.a(0))));
-       } else {
-           //entity = new EntityPlayer(server, server.getWorldServer(0), profile, new PlayerInteractManager(server.getWorldServer(0)));
-       }
-      // Player target = entity == null ? null : entity.getBukkitEntity();
-       if (target != null) {
-           target.loadData();
-           return target;
-       }
-       return target;
-   }
-   
+	/**
+	 * 
+	 * @param uuid
+	 * @return
+	 */
+	@SneakyThrows
+	public static Player getOfflinePlayer(UUID uuid) {
+		String version = "v" + Bukkit.getServer().getClass().getPackage().getName().split("v")[1];
+
+		Object profile = Class.forName("com.mojang.authlib.GameProfile").
+				getConstructor(UUID.class, String.class).
+				newInstance(uuid, Bukkit.getOfflinePlayer(uuid).getName());
+		
+		Object server  = Class.forName("net.minecraft.server".concat(version).concat("MinecraftServer")).
+				getDeclaredMethod("getServer").invoke(null);
+		
+		Object world   = server.getClass().getDeclaredMethod("getWorldServer").invoke(
+					Class.forName("net.minecraft.server".concat(version).concat("DimensionManager")).
+						getDeclaredMethod("a", int.class).invoke(null, 0)
+				);
+		
+		Object manager = Class.forName("net.minecraft.server".concat(version).concat("PlayerInteractManager")).
+				getConstructor(world.getClass()).newInstance(world);
+		
+		Object player  = Class.forName("net.minecraft.server".concat(version).concat("EntityPlayer")).
+				getConstructor(server.getClass(), world.getClass(), profile.getClass(), manager.getClass()).
+				newInstance(server, world, profile, manager);
+		
+		Player target = player == null ? null : Player.class.cast(player.getClass().getDeclaredMethod("getBukkitEntity").invoke(player));
+		
+        if (target != null)
+            target.loadData();
+        
+        return target;
+	}
+
 }
